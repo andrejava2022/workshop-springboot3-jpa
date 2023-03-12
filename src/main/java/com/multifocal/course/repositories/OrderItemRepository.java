8abcf194571b5c00d80f1e5af76c4f8566154a1a
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-03-12 11:30:30 -0300

    OrderItem, many-to-many association with extra attributes PARTE-2

---
+++ src/main/java/com/multifocal/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.multifocal.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.multifocal.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+	
+
+}
