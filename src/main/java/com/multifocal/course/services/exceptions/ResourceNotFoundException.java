bb537540131c9f8a34ea6f009277997e6323b13b
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-03-13 17:11:53 -0300

    Exception handling - findById

---
+++ src/main/java/com/multifocal/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.multifocal.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("ResourceNotFoundException. Id " + id);
+	}
+
+}
